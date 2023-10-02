6e2e23b807da8641373b72548a75f5bc2cf4b2dc
aokiji-zz <mwtteixeira@gmail.com>
2023-10-02 11:52:14 -0300

    take here

--- db/connect.go
+++ db/connect.go
@@ -3,14 +3,35 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type User struct {
+	user string
+	pass string
+}
+
+func getEnv() User {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln("Error loading .env %s", err)
+	}
+	user := os.Getenv("USR")
+	pass := os.Getenv("PASS")
+	admin := User{
+		user: user,
+		pass: pass,
+	}
+	return admin
+}
 func getConnection() (client *mongo.Client, ctx context.Context) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://your_root_username:your_root_password@localhost:27017"))
+	user := getEnv().user
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root_username:root_password@localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module web-crawler
 go 1.20
 
 require (
+	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.12.1
 	golang.org/x/net v0.15.0
 )
